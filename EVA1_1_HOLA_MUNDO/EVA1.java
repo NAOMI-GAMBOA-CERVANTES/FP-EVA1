27de51903e9ca54e10b633abdb79f267fc496cff
NAOMI-GAMBOA-CERVANTES <L24550254@chihuahua2.tecnm.mx>
2024-08-26 16:47:00 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/EVA1.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Nota
+        System.out.println("Hola Mundo!");//Cadena de texto
+        System.out.println(100);          //Número entero
+        System.out.println('c');          // Carácter
+        System.out.println(true);         //Valores lógicos
+        System.out.println(false);      
+        System.out.println(1000.52);      //Núm con fracción
+        
+        //TIPOS DE DATOS
+        //ENTEROS
+        //int
+        //long
+        //byte
+        //short
+        //PUNTO FLOTANTE
+        //double
+        //float
+        //TEXTO
+        //String
+        //CARACTER
+        //char
+        //LÓGICOS (true false)
+        //boolean
+        
+        //creación y uso de variables
+        int edad; //Declaro una varibla de tipo entero, llamado
+                    //edad
+        //double edad; //NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE
+        String nombre;
+        String apellido;
+        double promedio;
+        
+    }
+    
+}
