9a5520217868b1b81280b7a148d3493c7b2b0b88
NAOMI-GAMBOA-CERVANTES <L24550254@chihuahua2.tecnm.mx>
2024-08-26 16:52:05 -0600

    Add files via upload

---
+++ EVA1_2.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Nota
+        System.out.println("Hola Mundo!");//Cadena de texto
+        System.out.println(100);          //Número entero
+        System.out.println('c');          // Carácter
+        System.out.println(true);         //Valores lógicos
+        System.out.println(false);      
+        System.out.println(1000.52);      //Núm con fracción
+        
+        //TIPOS DE DATOS
+        //ENTEROS
+        //int
+        //long
+        //byte
+        //short
+        //PUNTO FLOTANTE
+        //double
+        //float
+        //TEXTO
+        //String
+        //CARACTER
+        //char
+        //LOGICOS (true false)
+        //boolean
+        
+        //creacion y uso de variables
+        int edad; //Declaro una variable de tipo entero, llamado
+                    //edad
+        //double edad; //NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE
+        String nombre;
+        String apellido;
+        double promedio;
+        
+        edad = 18;
+        
+        System.out.println(edad); //println tambi�n imprime variables
+    }
+    
+}
