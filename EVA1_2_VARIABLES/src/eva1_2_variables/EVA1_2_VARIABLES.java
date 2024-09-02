5f53e6f79d1f6d316a497ac42fb8678acee929b4
NAOMI-GAMBOA-CERVANTES <L24550254@chihuahua2.tecnm.mx>
2024-09-02 16:52:29 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_variables;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
