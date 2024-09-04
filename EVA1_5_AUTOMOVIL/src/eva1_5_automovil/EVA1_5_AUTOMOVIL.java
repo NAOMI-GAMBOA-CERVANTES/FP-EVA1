1dfaeb1db4526d4b149e9b68522de3f050f71d0e
NAOMI-GAMBOA-CERVANTES <L24550254@chihuahua2.tecnm.mx>
2024-09-04 16:33:26 -0600

    Add files via upload

---
+++ EVA1_5_AUTOMOVIL/src/eva1_5_automovil/EVA1_5_AUTOMOVIL.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_5_automovil;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_5_AUTOMOVIL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String modelo;
+        double precio;
+        int year;
+        //ASIGNAR VALORES
+        marca = "FORD";
+        year = 2020;
+        precio = 400999;
+        modelo = "Ford";
+        //IMPRIMIR
+        System.out.println("Marca: ");
+        System.out.println(marca);
+        System.out.println("Precio: ");
+        System.out.println(precio);
+        System.out.println("Año: ");
+        System.out.println(year);
+        System.out.println("Modelo: ");
+        System.out.println(modelo);
+    }
+    
+}
