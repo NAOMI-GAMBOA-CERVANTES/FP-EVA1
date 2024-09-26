b52b385f4ced5d9b88c87561c81791f4b45c3a19
NAOMI-GAMBOA-CERVANTES <L24550254@chihuahua2.tecnm.mx>
2024-09-26 16:51:39 -0600

    Add files via upload

---
+++ EVA1_18_IF_ELSE_ANIDADO/src/eva1_18_if_else_anidado/EVA1_18_IF_ELSE_ANIDADO.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_18_if_else_anidado;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_18_IF_ELSE_ANIDADO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
