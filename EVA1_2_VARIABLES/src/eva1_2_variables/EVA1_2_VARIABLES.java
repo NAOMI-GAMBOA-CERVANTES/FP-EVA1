c747b150eebc3af5bf57b9c642eef47e4c518438
NAOMI-GAMBOA-CERVANTES <L24550254@chihuahua2.tecnm.mx>
2024-09-27 16:21:51 -0600

    Update EVA1_2_VARIABLES.java

--- EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
+++ EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -17,6 +17,40 @@ public class EVA1_2_VARIABLES {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         System.out.println("Hola Mundo!");//Cadena de texto
+        System.out.println(100);          //NÃºmero entero
+        System.out.println('c');          // CarÃ¡cter
+        System.out.println(true);         //Valores lÃ³gicos
+        System.out.println(false);      
+        System.out.println(1000.52);      //NÃºm con fracciÃ³n
+        
+        //TIPOS DE DATOS
+        //ENTEROS
+        //int
+        //long
+        //byte
+        //short
+        //PUNTO FLOTANTE
+        //double
+        //float
+        //TEXTO
+        //String
+        //CARACTER
+        //char
+        //LOGICOS (true false)
+        //boolean
+        
+        //creacion y uso de variables
+        int edad; //Declaro una variable de tipo entero, llamado
+                    //edad
+        //double edad; //NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE
+        String nombre;//String
+        String apellido;
+        double promedio;
+        
+        edad = 18;
+        
+        System.out.println(edad); //println también imprime variables
     }
     
 }
